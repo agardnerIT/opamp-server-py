84e5965a230e48cd2a650f8532d43a39c11d1bb5
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2025-08-18 18:25:21 +1000

    fix typo. Relates to #12

--- env_vars.py
+++ env_vars.py
@@ -2,4 +2,4 @@ import os
 
 SERVER_HTTP_SCHEME = os.environ.get("SERVER_HTTP_SCHEME", "http")
 SERVER_ADDR = os.environ.get("SERVER_ADDRESS", "localhost")
-SERVER_PORT = os.environ.get("SERVER_POST", "4320")
+SERVER_PORT = os.environ.get("SERVER_PORT", "4320")

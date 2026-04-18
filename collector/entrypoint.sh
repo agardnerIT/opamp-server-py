5661ecbadb95ed7bebccdb50c9b0525c85714127
agardnerit <adam@agardner.net>
2026-04-18 12:19:23 +1000

    remove file

--- collector/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -f /etc/otelcol-contrib/custom_config.yaml ]; then
-  exec /otelcolcontrib --config=/etc/otelcol-contrib/custom_config.yaml "$@"
-else
-  exec /otelcolcontrib --config=/etc/otelcol-contrib/config.yaml "$@"
-fi
